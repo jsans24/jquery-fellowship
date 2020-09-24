be42a20d95f07fde7375d2f42a26a06332168ffa
jsans24 <jsans24@gmail.com>
2020-09-23 22:37:57 -0700

    Chapter 7 complete - Strider is changed to Aragorn

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -201,6 +201,12 @@ const beautifulStranger = () => {
 
   // 1. change the buddy 'Strider' textnode to "Aragorn"
 
+  const $strider = $('.buddy')[3];
+
+  console.log($strider);
+
+  $strider.innerHTML = 'Aragorn';
+
   // hint: You can get a list of elements by tag name, such as 'aside'
 
 };
